0e091d2b53f075557a5451098a47e36d922c9956
corentin-vasseur <corentin.vasseur@axa.com>
2025-11-04 10:47:58 +0100

    fix: add env viarbale in deploy setp

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-  site: 'https://ai-helpers.github.io',
-  base: process.env.NODE_ENV === 'production' ? '/kscs-website' : '/',
-});
+// export default defineConfig({
+//   site: 'https://ai-helpers.github.io',
+//   base: process.env.NODE_ENV === 'production' ? '/kscs-website' : '/',
+// });
