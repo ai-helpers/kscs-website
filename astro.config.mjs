901fd544af943ed8e1c9bfffdacf6445e0d52bf6
corentin-vasseur <corentin.vasseur@axa.com>
2025-11-04 11:50:00 +0100

    fix(test): redirection

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,9 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ai-helpers.github.io',
-  base: '/',
-  trailingSlash: 'always'
+  base: '/kscs-website',
+  trailingSlash: 'always',
+  redirects: {
+    '/doc/[...slug]': '/kscs-website/doc/[..slug]'
+  }
 });
