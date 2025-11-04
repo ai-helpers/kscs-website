88d1e536cd109ad024738b3e8b0a0320e49ed6af
corentin-vasseur <corentin.vasseur@axa.com>
2025-11-04 11:17:44 +0100

    fix: exposition

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ai-helpers.github.io',
-  base: '/kscs-website',
   trailingSlash: 'always'
 });
